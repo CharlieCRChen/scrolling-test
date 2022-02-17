d3fbd34c9524151f2f9781e84dce78f4be74fe4f
CharlieCRChen <87412353+CharlieCRChen@users.noreply.github.com>
2022-02-17 01:41:26 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -272,8 +272,12 @@ $("#svg-container").scroll(function() {
 
 function test() {  
     var offset = $(".star").offset();
-    var y = parseInt(offset.top-$( window ).scrollTop());
-    console.log([offset.top, $( window ).scrollTop(), $( "#svg-container" ).scrollTop(), y]);
+    //var y = parseInt(offset.top-$( window ).scrollTop());
+
+    var y = parseInt($(".star").offsetTop()-$( "#svg-container" ).scrollTop())
+    
+    console.log([$(".star").offsetTop(), $( "#svg-container" ).scrollTop(), y]);
+    
     if(document.documentElement.scrollTop == topValue && y>navbar_height-5 && y<navbar_height+10) {
         if (shuffled1.length == 0 && SHOW_STAR == false){
             var temp_line_index = insert_symbol_line+1;
